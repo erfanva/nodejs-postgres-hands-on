4fa16e6447c6949e2d792f8daa2af2b4c1c8f7eb
erfanva <erfanv1@gmail.com>
2023-08-21 17:11:03 +0330

    smaple order

---
+++ create_sample_order.sh
@@ -0,0 +1,31 @@
+#### smaple order:
+curl --location '127.0.0.1:3000/orders/costumer' \
+--header 'Content-Type: application/json' \
+--data '{
+    "costumer_id":1,
+    "vendor_id":1
+}'
+curl --location '127.0.0.1:3000/orders/costumer' \
+--header 'Content-Type: application/json' \
+--data '{
+    "costumer_id":1,
+    "vendor_id":1
+}'
+curl --location '127.0.0.1:3000/orders/costumer' \
+--header 'Content-Type: application/json' \
+--data '{
+    "costumer_id":1,
+    "vendor_id":2
+}'
+curl --location '127.0.0.1:3000/orders/costumer' \
+--header 'Content-Type: application/json' \
+--data '{
+    "costumer_id":1,
+    "vendor_id":3
+}'
+curl --location '127.0.0.1:3000/orders/costumer' \
+--header 'Content-Type: application/json' \
+--data '{
+    "costumer_id":1,
+    "vendor_id":2
+}'
