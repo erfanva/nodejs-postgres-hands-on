38782628eaf3b31198f226657f788c9051741184
erfanva <erfanv1@gmail.com>
2023-08-21 10:01:41 +0330

    migrate_vendors, migrate_orders

--- src/db/migrations/index.js
+++ src/db/migrations/index.js
@@ -19,8 +19,41 @@ async function migrate_agents() {
         await db.query(format(insert_query, agents))
 }
 
+async function migrate_vendors() {
+    const create_query = `
+        CREATE TABLE IF NOT EXISTS vendors
+        (
+            id SERIAL,
+            PRIMARY KEY(id),
+            name varchar(30) NOT NULL
+        );`
+    const emptiness_check_query = `SELECT * FROM vendors LIMIT 1`;
+    const insert_query = `
+        INSERT INTO vendors(name)
+        VALUES %L;`
+    const agents = [['halal food'], ['sib360'], ['mohsen']]
+
+    await db.query(create_query)
+    if((await db.query(emptiness_check_query)).rowCount == 0) 
+        await db.query(format(insert_query, agents))
+}
+
+async function migrate_orders() {
+    const create_query = `
+        CREATE TABLE IF NOT EXISTS orders
+        (
+            id SERIAL,
+            PRIMARY KEY(id),
+            vendor_id integer REFERENCES vendors (id),
+            delivery_time integer
+        );`
+    await db.query(create_query)
+}
+
 module.exports = {
     migrate_all: () => {
         migrate_agents()
+        migrate_vendors()
+        migrate_orders()
     }
 }
