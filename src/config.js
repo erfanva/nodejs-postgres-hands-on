54d5fd3089d99a6425d9cc6a28045bcb92417eda
erfanva <erfanv1@gmail.com>
2023-08-24 03:07:25 +0330

    .env

--- src/config.js
+++ src/config.js
@@ -1,19 +1,21 @@
 require('dotenv').config()
 
+console.log(process.env.db_pass)
+
 module.exports = {
     app: {
-        host: process.env.host || '0.0.0.0',
-        port: process.env.port || 3000,
+        host: process.env.app_host || '0.0.0.0',
+        port: process.env.app_port || 3000,
         is_developing: true,
     },
     apis: {
         eta: 'https://run.mocky.io/v3/122c2796-5df4-461c-ab75-87c1192b17f7',
     },
     db: {
-        user: 'me',
-        host: '127.0.0.1',
-        database: 'api',
-        password: 'password',
-        port: 5432,
+        user: process.env.db_user,
+        host: process.env.db_host,
+        database: process.env.db_name,
+        password: process.env.db_pass,
+        port: process.env.db_port,
     }
 }
